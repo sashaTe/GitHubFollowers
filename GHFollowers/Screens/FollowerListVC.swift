b9d2d3e07886698577101d7076e5229cbaf534dc
Alexander Zarutskiy <sashasolo23@gmail.com>
2023-09-14 17:31:39 +0100

    FollowerLIstVC refactor, UICollectionVIew setup

--- GHFollowers/Screens/FollowerListVC.swift
+++ GHFollowers/Screens/FollowerListVC.swift
@@ -10,18 +10,15 @@ import UIKit
 class FollowerListVC: UIViewController {
     
     var username: String!
+    var collectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBackground
-        navigationController?.navigationBar.prefersLargeTitles = true
+        configureCollectionView()
+        configureViewController()
+        getFollowers()
         
-        NetworkManager.shared.getFollowers(for: username, page: 1) { result in
-            switch result {
-            case .success(let followers): print(followers)
-            case .failure(let error): self.presentGFAlertOnMainThread(title: "Bad Stuff Happend", message: error.rawValue, buttonTitle: "OK")
-            }
-        }
+      
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -30,6 +27,25 @@ class FollowerListVC: UIViewController {
         
     }
     
-
+   func configureViewController() {
+       view.backgroundColor = .systemBackground
+       navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    func configureCollectionView() {
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UICollectionViewFlowLayout())
+        view.addSubview(collectionView)
+        collectionView.backgroundColor = .systemPink
+        collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
+    }
+    
+    func getFollowers() {
+        NetworkManager.shared.getFollowers(for: username, page: 1) { result in
+            switch result {
+            case .success(let followers): print(followers)
+            case .failure(let error): self.presentGFAlertOnMainThread(title: "Bad Stuff Happend", message: error.rawValue, buttonTitle: "OK")
+            }
+        }
+    }
 
 }
